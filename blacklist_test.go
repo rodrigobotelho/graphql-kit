e3155dbe3067e75f148f6bbbb787d60743ef03e1
Marcos César de Oliveira <mcesarhm@gmail.com>
2018-12-26 10:39:52 -0200

    Add test to exercise last commit

---
+++ blacklist_test.go
@@ -0,0 +1,27 @@
+package graphqlkit
+
+import "testing"
+
+func Test_findOpName(t *testing.T) {
+	type args struct {
+		req string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"More than one space after bracket",
+			args{"{  qn }"},
+			"QN",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findOpName(tt.args.req); got != tt.want {
+				t.Errorf("findOpName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
